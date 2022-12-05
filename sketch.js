b2c2bd42dcfaccd380ebd9b7edfe0d125ec895d7
Manuel <github@mmvv.pw>
2022-12-05 11:13:59 -0600

    moving circles

--- sketch.js
+++ sketch.js
@@ -1,50 +1,98 @@
+let canvasWidth = 444,
+    canvasCenter = canvasWidth/2
 // circle positions
-let xPosOne = 90,
-  xPosTwo = xPosOne + 60,
-  xPosThree = xPosTwo + 60,
+  xPosOne = canvasCenter,
+  xPosTwo = canvasCenter,
+  xPosThree = canvasCenter,
+
   // circle heights
   heightOne = 80,
-  heightTwo = heightOne + 100,
-  heightThree = heightTwo + 100,
+  heightTwo = heightOne + 150,
+  heightThree = heightTwo + 250,
+
   //circle diameters
   circleSize = 80,
+
   // colors
   lightGoldBorder = "#DDDF74",
   darkGoldBorder = "#D8DA90",
-  lightBackgroundColor = "#CBDEF6", //light blue
+  lightBackgroundColor = "#8bb7eb", //light blue
   darkBackgroundColor = "#252525"; //light black
 
 function setup() {
-  createCanvas(666, 444);
+  createCanvas(canvasWidth, 666);
   background(lightBackgroundColor);
   //noStroke();   // don't draw shape outlines pls
 
   diameter = width / 2;
   console.log(diameter);
 
-  strokeWeight(13);
+  strokeWeight(5);
 
-  stroke(darkGoldBorder); //dark yellow border
+  //stroke(darkGoldBorder); //dark yellow border
 
   // Big Circle 1 - Blue Teal
-  fill("#B5F0EE"); //Circle Color - Light Blue
-  circle(xPosThree * 2, heightThree, circleSize * 3);
+  //fill("#B5F0EE"); //Circle Color - Light Blue
+  //circle(xPosThree, heightThree, circleSize * 3);
 
-  stroke(lightGoldBorder); //light yellow border
+  //stroke(lightGoldBorder); //light yellow border
 
   // Big Circle 2 - Violet
-  fill("#BE72E1"); //Circle Color - Light Blue
-  circle(xPosThree, heightTwo, circleSize * 2);
+  //fill("#BE72E1"); //Circle Color - Light Blue
+  //circle(xPosTwo, heightTwo, circleSize * 2);
 
-  stroke(darkGoldBorder); //dark yellow border
+  //stroke(darkGoldBorder); //dark yellow border
 
   // Big Circle 3 - Pink
-  fill("#F87EC2"); //Circle Color - Pink
-  circle(xPosOne, heightOne, circleSize);
+  //fill("#F87EC2"); //Circle Color - Pink
+  //circle(xPosThree, heightOne, circleSize);
 }
 
 function draw() {
-  //fill('#0096FF'); //object color blue
+
+  //move top circle right until border
+  if(xPosThree < 368){
+    xPosThree += 1;
+  }
+
+  //move middle left circle until border
+  if(xPosTwo > 86){
+    xPosTwo -= 1;
+  }
+
+  if(xPosThree == 368 && heightThree < 538){
+    heightThree += 1
+  }
+
+  if(heightThree == 538){
+    noLoop()  // stop draw() once complete
+  }
+
+  stroke(darkGoldBorder); //dark yellow border
+
+  // Draw Circle 1 with new Y position - Blue Teal
+  fill("#B5F0EE"); //Circle Color - Light Blue
+  circle(xPosOne, heightThree, circleSize * 3);
+
+  stroke(lightGoldBorder); //light yellow border
+
+
+  // Draw Circle 2 with new X position - Violet
+  fill("#BE72E1"); //Circle Color - Light Blue
+  circle(xPosTwo, heightTwo, circleSize * 2);
+
+  stroke(darkGoldBorder); //dark yellow border
+  
+  // Draw Circle 3 with new X position - Pink
+  fill("#F87EC2"); //Circle Color - Pink
+  circle(xPosThree, heightOne, circleSize);
+
+
+  console.log('xPosOne: ' + xPosOne)
+  console.log('xPosTwo: ' + xPosTwo)
+  console.log('xPosThree: ' + xPosThree)
+
+
 }
 
 //radial gradient class
