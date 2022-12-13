7e3877610ac2ccd135737bf68b4affef25fb9704
Manuel <github@mmvv.pw>
2022-12-13 11:49:12 -0600

    more refactoring

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-let canvasWidth = 444 
+let canvasWidth = 444
   canvasHeight = 666
   canvasCenter = canvasWidth/2
 
@@ -30,13 +30,13 @@ let canvasWidth = 444
   rightBorder = canvasWidth - 76
   leftBorder = 86
   bottomBorder = 538
-  radialBorder = 797
+  radialBorder = 808
 
 function setup() {
+  console.info('windowWidth: ' + windowWidth)
   createCanvas(canvasWidth, canvasHeight)
   background(lightBackgroundColor)
   strokeWeight(5)
-  //noStroke()  // don't draw shape outlines pls  
 }
 
 function draw() {
@@ -47,22 +47,26 @@ function draw() {
 
 function updateValues(){
     //move top circle right until border
-    if(xPosThree < rightBorder){
-      xPosThree += 1
+    if(xPosOne < rightBorder){
+      xPosOne += 1
+      console.log('xPosOne: ' + xPosOne)
     }
   
     //move middle circle left until border
     if(xPosTwo > leftBorder){
       xPosTwo -= 1
+      console.log('xPosTwo: ' + xPosTwo)
     }
   
     //move bottom circle down until border only after top circle reaches border
-    if(xPosThree >= rightBorder && heightThree < bottomBorder){
+    if(xPosOne >= rightBorder && heightThree < bottomBorder){
       heightThree += 1
+      console.log('heightThree: ' + heightThree)
     }
 
     if(heightThree > bottomBorder && circleSizeFour <= radialBorder){
       circleSizeFour += 1
+      console.log('circleSizeFour: ' + circleSizeFour)
     }
   
     // after last circle reaches the bottom border, stop draw() loop
@@ -72,35 +76,37 @@ function updateValues(){
 }
 
 function updateCanvas(){
+
   stroke(darkGoldBorder) //dark yellow border
 
   // Draw Circle 1 with new Y position - Teal
   fill("#B5F0EE")
-  circle(xPosOne, heightThree, circleSizeThree)
+  circle(xPosThree, heightThree, circleSizeThree)
 
   stroke(lightGoldBorder) //light yellow border
 
   // Draw Circle 2 with new X position - Violet
-  fill("#BE72E1");
+  fill("#BE72E1")
   circle(xPosTwo, heightTwo, circleSizeTwo)
 
-  stroke(darkGoldBorder); //dark yellow border
+  stroke(darkGoldBorder) //dark yellow border
 
   // Draw Circle 3 with new X position - Pink
-  fill("#F87EC2"); //Circle Color - Pink
-  circle(xPosThree, heightOne, circleSizeOne)
+  fill("#F87EC2") //Circle Color - Pink
+  circle(xPosOne, heightOne, circleSizeOne)
 
-  //Draw Circle 4
+  // Draw Circle 4
   noStroke()
-  fill(darkBackgroundColor);
+  if(circleSizeFour > radialBorder){
+    fill("#FFF")
+  }else{
+    fill(darkBackgroundColor)
+  }
   circle(xPosFour, heightFour, circleSizeFour)
 }
 
 function updateLogs(){
-  console.log('xPosOne: ' + xPosOne)
-  console.log('xPosTwo: ' + xPosTwo)
-  console.log('xPosThree: ' + xPosThree)
-  console.log('xPosFour: ' + xPosFour)
+  console.log('frameCount: ' + frameCount)
 }
 
 //radial gradient class
